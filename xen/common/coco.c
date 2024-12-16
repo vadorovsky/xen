4d9476e702629045bb399363f2d304bb6f8616b6
Michal Rostecki <vadorovsky@protonmail.com>
2024-12-16 16:55:39 +0100

    coco: Unpack guest arguments correctly
    Adjust the signature of the `do_dom_coco_op` function to the usual
    hypercall signature, where the second argument after command code
    is `XEN_GUEST_HANDLE_PARAM(void) arg`. Then copy the argument struct
    correctly using `copy_from_guest` and then pass the individual fields
    further.
    
    Remove the `cmd` field from the `sev_launch_update_data` struct. It's
    not needed there, hypercall commands are passed separately as the
    first argument.

--- xen/common/coco.c
+++ xen/common/coco.c
@@ -3,14 +3,21 @@
  */
 
 #include <xen/types.h>
+#include <public/hvm/coco.h>
 #include <xen/errno.h>
 #include <xen/domain.h>
+#include <xen/guest_access.h>
 
 long
-do_dom_coco_op(unsigned int cmd, domid_t domid, uint64_t arg1, uint64_t arg2)
+do_dom_coco_op(unsigned int cmd, XEN_GUEST_HANDLE_PARAM(void) arg)
 {
 #ifdef CONFIG_X86
-    return arch_dom_coco_op(cmd, domid, arg1, arg2);
+    sev_launch_update_data_t data = {};
+
+    if ( unlikely(copy_from_guest(&data, arg, 1)) )
+        return -EFAULT;
+
+    return arch_dom_coco_op(cmd, data.domid, data.address, data.len);
 #else
     return -ENOSYS;
 #endif
--- xen/include/public/hvm/coco.h
+++ xen/include/public/hvm/coco.h
@@ -5,7 +5,6 @@
 
 /* Map to the sev_dom_coco_op */
 struct sev_launch_update_data {
-    unsigned int cmd;
     domid_t domid;
     uint64_t address;
     uint64_t len;
@@ -13,7 +12,5 @@ struct sev_launch_update_data {
 
 typedef struct sev_launch_update_data sev_launch_update_data_t;
 DEFINE_XEN_GUEST_HANDLE(sev_launch_update_data_t);
-
-extern long do_dom_coco_op(unsigned int cmd, domid_t domid, uint64_t arg1, uint64_t arg2);
 
 #endif /* __XEN_PUBLIC_HVM_COCO_H__ */
